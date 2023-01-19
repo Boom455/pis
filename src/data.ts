95fb107bd94ac78d620fd597167e88fd4c945798
Boom455 <boomchooboom@gmail.com>
2023-01-19 22:11:28 +0700

    rittersport

---
+++ src/data.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+const product_list: { [key: string]: any } = {}
+
+for (const file of fs.readdirSync(`${process.cwd()}/data`)) {
+    const code = file.replace('.json', '')
+    console.log('Import product', code)
+    product_list[code] = JSON.parse(fs.readFileSync(`${process.cwd()}/data/${file}`, 'utf8'))
+}
+
+const product_type: { [key: number]: string } = {
+    0: 'OTHER',
+    1: 'FOOD AND DRINK',
+    2: 'DRINKFOOD AND DRINK',
+    3: 'HOUSEHOLD ESSENTIALS',
+    4: 'ELECTRONIC',
+    5: 'BEAUTY AND PERSONAL CARE',
+    6: 'HEALTH AND WELLNESS',
+    7: 'PETS FOOD',
+    8: 'SEASONING SAUCE',
+    9: 'MOM AND BABY',
+    10: 'PETS SUPPLIES',
+}
+
+export default { product_list, product_type }
